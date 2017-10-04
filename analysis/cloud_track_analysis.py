a654f1d4b45268a8165d65bb7b69832e9aa826d0
Mark Muetzelfeldt <markmuetz@gmail.com>
2017-10-04 15:31:07 +0100

    Change cloud tracking to use display_results.

--- analysis/cloud_track_analysis.py
+++ analysis/cloud_track_analysis.py
@@ -9,7 +9,7 @@ from omnium.analyser import Analyser
 from omnium.utils import get_cube_from_attr
 from cloud_tracking.utils import label_clds
 from cloud_tracking import Tracker
-from cloud_tracking.cloud_tracking_analysis import output_stats
+from cloud_tracking.cloud_tracking_analysis import generate_stats, plot_stats, output_stats_to_file
 
 logger = getLogger('scaf.cta')
 
@@ -34,6 +34,8 @@ class CloudTrackAnalyser(Analyser):
     def run_analysis(self):
         cubes = self.cubes
 
+        self.trackers = {}
+        self.all_stats = {}
         cloud_mask_id = 'cloud_mask'
         cloud_mask_cube = get_cube_from_attr(cubes, 'omnium_cube_id', cloud_mask_id)
 
@@ -58,17 +60,29 @@ class CloudTrackAnalyser(Analyser):
                     cld_field[time_index] = cld_labels
                 cld_field_cube.data = cld_field
 
-                tracker = Tracker(cld_field_cube.slices_over('time'), show_working=True)
+                tracker = Tracker(cld_field_cube.slices_over('time'), store_working=True)
                 tracker.track()
-                # proj_cld_field_cube = cld_field_cube.copy()
-                # proj_cld_field_cube.data = tracker.proj_cld_field.astype(float)
-                # iris.save(proj_cld_field_cube, 'output/{}_proj_cld_field.nc'.format(expt))
-
                 tracker.group()
-                # TODO: Hacky.
-                # Force creation of output_dir (hacky).
-                dummy_path = self.figpath('tracks')
-                output_dir = os.path.dirname(dummy_path)
-                prefix = '{}_z{}_t{}_'.format(os.path.basename(dummy_path), height_level_index, thresh_index)
-                stats_for_expt = output_stats({self.expt: tracker}, output_dir, prefix=prefix)
-                stats = stats_for_expt[self.expt]
+
+                stats = generate_stats(self.expt, tracker)
+                self.trackers[(height_level_index, thresh_index)] = tracker
+                self.all_stats[(height_level_index, thresh_index)] = stats
+
+    def display_results(self):
+        self.append_log('displaying results')
+        figpath = self.figpath('cloud_tracking')
+
+        cubes = self.cubes
+        cloud_mask_id = 'cloud_mask'
+        cloud_mask_cube = get_cube_from_attr(cubes, 'omnium_cube_id', cloud_mask_id)
+        w_thresh_coord = cloud_mask_cube.coord('w_thres')
+        level_number_coord = cloud_mask_cube.coord('model_level_number')
+        for height_level_index, height_level in enumerate(level_number_coord.points):
+            for thresh_index in range(w_thresh_coord.shape[0]):
+                stats = self.all_stats[(height_level_index, thresh_index)]
+                tracker = self.trackers[(height_level_index, thresh_index)]
+
+                filename = 'atmos.cloud_tracking_z{}_t{}.'.format(height_level_index, thresh_index)
+
+                plot_stats(self.expt, os.path.dirname(figpath), filename, [stats])
+                output_stats_to_file(self.expt, os.path.dirname(figpath), filename + 'txt', tracker, stats)
