c7a7630a1156e370787919132ee1350995348ba3
Mark Muetzelfeldt <markmuetz@gmail.com>
2018-07-03 10:47:20 +0100

    optimize imports and delete no longer needed analysis

--- scaffold/__init__.py
+++ scaffold/__init__.py
@@ -1,24 +1,27 @@
 from scaffold.version import VERSION
+from scaffold.scaffold_settings import settings as analysis_settings
+
 from scaffold.cycle.cloud_analysis import CloudAnalyser
 from scaffold.cycle.mass_flux_analysis import MassFluxAnalyser
 from scaffold.cycle.mass_flux_spatial_scales_analysis import MassFluxSpatialScalesAnalyser
 from scaffold.cycle.org_analysis import OrgAnalyser
 from scaffold.cycle.profile_analysis import ProfileAnalyser
 from scaffold.cycle.restart_dump_analysis import RestartDumpAnalyser
+
 from scaffold.expt.cloud_track_analysis import CloudTrackAnalyser
-from scaffold.dummy_analysis import DummyAnalyser
 from scaffold.expt.mass_flux_combined import MassFluxCombinedAnalysis
-from scaffold.suite.mass_flux_plot import MassFluxPlotter
 from scaffold.expt.mass_flux_spatial_scales_combined import MassFluxSpatialScalesCombined
-from scaffold.suite.mass_flux_spatial_scales_plot import MassFluxSpatialScalesPlotter
 from scaffold.expt.mse_combined import MseCombinedAnalysis
-from scaffold.suite.mse_plot import MsePlotter
 from scaffold.expt.org_combined import OrgCombined
+from scaffold.expt.precip_hovmoller import PrecipHovmollerAnalyser
+from scaffold.expt.surf_flux_analysis import SurfFluxAnalyser
+
+from scaffold.suite.mass_flux_plot import MassFluxPlotter
+from scaffold.suite.mass_flux_spatial_scales_plot import MassFluxSpatialScalesPlotter
+from scaffold.suite.mse_plot import MsePlotter
 from scaffold.suite.org_plot import OrgPlotter
 from scaffold.suite.precip_plot import PrecipPlot
-from scaffold.expt.precip_hovmoller import PrecipHovmollerAnalyser
 from scaffold.suite.profile_plot import ProfilePlotter
-from scaffold.expt.surf_flux_analysis import SurfFluxAnalyser
 from scaffold.suite.surf_flux_plot import SurfFluxPlot
 
 
@@ -27,7 +30,6 @@ __version__ = VERSION
 analysis_classes = [
     CloudAnalyser,
     CloudTrackAnalyser,
-    DummyAnalyser,
     MassFluxAnalyser,
     MassFluxCombinedAnalysis,
     MassFluxPlotter,
--- scaffold/dummy_analysis.py
+++
@@ -1,9 +0,0 @@
-from omnium.analyser import Analyser
-
-
-class DummyAnalyser(Analyser):
-    analysis_name = 'dummy_analysis'
-    single_file = True
-
-    def run_analysis(self):
-        pass
