78f220331d5546cf6b9dc91633353e61b1190686
Mark Muetzelfeldt <markmuetz@gmail.com>
2018-07-12 13:45:32 +0100

    Make expt_converter OK with missing files by using glob.

--- scaffold/expt/expt_converter.py
+++ scaffold/expt/expt_converter.py
@@ -5,7 +5,8 @@ class ExptConverter(FF2NC_Converter):
     analysis_name = 'expt_converter'
     single_file = True
     input_dir = 'work/20000101T0000Z/{expt}_atmos'
-    input_filename = '{input_dir}/atmos.pp3'
+    # A glob so that if it's not there it doesn't raise an error.
+    input_filename_glob = '{input_dir}/atmos.pp3'
     output_dir = 'work/20000101T0000Z/{expt}_atmos'
     output_filenames = ['{output_dir}/atmos.pp3.nc']
 
