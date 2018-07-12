40c0bc5c369de9ff904c7a9d7cff79efc0a11120
Mark Muetzelfeldt <markmuetz@gmail.com>
2018-07-12 17:54:50 +0100

    Check all analysis enabled and copy to test dir.

--- scaffold/tests/test_suite_u-an388/test_run_all_analysis.py
+++ scaffold/tests/test_suite_u-an388/test_run_all_analysis.py
@@ -1,6 +1,9 @@
 import os
 import shutil
+from configparser import ConfigParser
+
 import omnium
+import scaffold
 
 
 def setup_module():
@@ -9,6 +12,27 @@ def setup_module():
     assert os.path.exists(suite_dir)
     if os.path.exists(os.path.join(suite_dir, 'omnium_output')):
         shutil.rmtree(os.path.join(suite_dir, 'omnium_output'))
+    rose_app_conf_fn = os.path.join(os.path.dirname(os.path.dirname(scaffold.__file__)),
+                                    'rose-app.conf')
+    shutil.copy(rose_app_conf_fn, os.path.join(suite_dir, 'app/omnium', 'rose-app.conf' ))
+
+
+def test_00_check_all_analysers_in_use():
+    suite_dir = os.getenv('SCAFFOLD_SUITE_UAN388_DIR')
+    os.chdir(suite_dir)
+    cp = ConfigParser()
+    cp.read('app/omnium/rose-app.conf')
+    analysis_names = []
+    for run_type in ['cycle', 'expt', 'suite']:
+        runcontrol_sec = 'runcontrol_{}'.format(run_type)
+        runcontrol = cp[runcontrol_sec]
+        for ordered_analysis, enabled_str in sorted(runcontrol.items()):
+            assert enabled_str == 'True'
+            analysis_name = ordered_analysis[3:]
+            analysis_names.append(analysis_name)
+    scaffold_analysis_names = [a.analysis_name for a in scaffold.analysis_classes]
+    assert set(analysis_names) == set(scaffold_analysis_names)
+
 
 def test_01_run_cycle_analysis():
     suite_dir = os.getenv('SCAFFOLD_SUITE_UAN388_DIR')
