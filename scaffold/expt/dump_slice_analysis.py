628f836e07e3b19f26a98b9ffddbc3db3581cd2a
Mark Muetzelfeldt <markmuetz@gmail.com>
2018-09-26 12:08:19 +0100

    Loop over qvars doing mean over y plot for each.

--- scaffold/expt/dump_slice_analysis.py
+++ scaffold/expt/dump_slice_analysis.py
@@ -1,10 +1,11 @@
+import os
 from logging import getLogger
 
 import numpy as np
 import scipy
 import matplotlib.pyplot as plt
 
-from omnium import Analyser
+from omnium import Analyser, OmniumError
 from omnium.consts import Re, L, cp, g
 from omnium.utils import get_cube
 
@@ -39,29 +40,49 @@ class DumpSliceAnalyser(Analyser):
         self.qcf = get_cube(dump, 0, 12)
         self.qrain = get_cube(dump, 0, 272)
         self.qgraup = get_cube(dump, 0, 273)
+        try:
+            qcf2 = get_cube(dump, 0, 271)
+            self.qcf2 = qcf2
+        except OmniumError:
+            logger.info('dump has no qcf2')
 
     def display_results(self):
+        os.makedirs(self.file_path('/xy'), exists_ok=True)
+        os.makedirs(self.file_path('/xz'), exists_ok=True)
+        os.makedirs(self.file_path('/yz'), exists_ok=True)
+        self.vertlevs = VertLev(self.suite.suite_dir)
         self._plot(self.task.expt)
 
     def _plot(self, expt):
-        vertlevs = VertLev(self.suite.suite_dir)
-        fig, ax = plt.subplots(dpi=100)
-        data = self.qcl.data
-        # Coords are model_level, y, x or model_level, lat, lon
-        data_mean = data.mean(axis=1)
-        Nx = data.shape[2]
-        data_rbs = scipy.interpolate.RectBivariateSpline(vertlevs.z_theta, np.arange(Nx),
-                                                         data_mean)
-        data_interp = data_rbs(np.linspace(0, 40000, 400), np.linspace(0, Nx - 1, Nx))
-        # Only go up to 20 km and use aspect ratio to plot equal aspect (allowing for diff in coords).
-        im = ax.imshow(data_interp[:200], origin='lower', cmap='Blues', aspect=0.1)
+        self._qvar_plots(expt)
 
-        ax.set_title('')
-        ax.set_xlabel('x (km)')
-        ax.set_ylabel('height (100 m)')
-        plt.colorbar(im)
-        plt.savefig(self.file_path('{}_{}_slice_mean_over_y.png'.format(expt, self.task.runid)))
-        plt.close('all')
+    def _qvar_plots(self, expt):
+        qvars = ['qcl', 'qcf', 'qrain', 'qgraup']
+        for qvar in qvars:
+            if not hasattr(self, qvar):
+                continue
+            qcube = getattr(self, qvar)
+
+            fig, ax = plt.subplots(dpi=100)
+            data = qcube.data
+            # Coords are model_level, y, x or model_level, lat, lon
+            data_mean = data.mean(axis=1)
+            Nx = data.shape[2]
+            data_rbs = scipy.interpolate.RectBivariateSpline(self.vertlevs.z_theta, np.arange(Nx),
+                                                             data_mean)
+            data_interp = data_rbs(np.linspace(0, 40000, 400), np.linspace(0, Nx - 1, Nx))
+            # Only go up to 20 km and use aspect ratio to plot equal aspect
+            # (allowing for diff in coords).
+            im = ax.imshow(data_interp[:200], origin='lower', cmap='Blues', aspect=0.1)
+
+            ax.set_title('{} mean over y'.format(qvar))
+            ax.set_xlabel('x (km)')
+            ax.set_ylabel('height (100 m)')
+            plt.colorbar(im)
+            plt.savefig(self.file_path('/xz/{}_{}_{}_mean_over_y.png'.format(expt,
+                                                                             self.task.runid,
+                                                                             qvar)))
+            plt.close('all')
 
     def save(self, state, suite):
         with open(self.task.output_filenames[0], 'w') as f:
