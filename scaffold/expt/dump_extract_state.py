75fb9f4a251c4dc9fa543aa76fe3bb356eb9457f
Mark Muetzelfeldt <markmuetz@gmail.com>
2019-01-08 15:20:01 +0000

    Flesh out dump_extract_state.

--- scaffold/expt/dump_extract_state.py
+++ scaffold/expt/dump_extract_state.py
@@ -1,4 +1,5 @@
 from logging import getLogger
+import configparser as cp
 
 import iris
 
@@ -24,6 +25,9 @@ class DumpExtractState(Analyser):
         theta = []
         mv = []
         for dump_filename in filenames:
+            if self.suite.check_filename_missing(dump_filename):
+                logger.debug('filename {} missing, skipping', dump_filename)
+                continue
             da = iris.load(dump_filename)
             da_theta = get_cube(da, 0, 4)
             da_mv = get_cube(da, 0, 391)
@@ -32,12 +36,34 @@ class DumpExtractState(Analyser):
 
         self.theta = iris.cube.CubeList.merge(theta)
         self.mv = iris.cube.CubeList.merge(mv)
+        self.z = self.theta.coord('atmosphere_hybrid_height_coordinate').points
 
     def run(self):
-        pass
+        self.theta_profile = self.theta.data.mean(axis=(0, 2, 3))
+        self.mv_profile = self.mv.data.mean(axis=(0, 2, 3))
 
     def display_results(self):
-        pass
+        opt = cp.ConfigParser()
+        opt.add_section('namelist:idealised')
+
+        opt.set('namelist:idealised', 'mv_relax_data', ','.join(['{:.5f}'.format(v) for v in self.mv_profile]))
+        opt.set('namelist:idealised', 'mv_relax_height', ','.join(['{:.2f}'.format(v) for v in self.z]))
+        opt.set('namelist:idealised', 'mv_relax_time', '0')
+        opt.set('namelist:idealised', 'mv_relax_timescale', '25600.0')
+
+        opt.set('namelist:idealised', 'num_mv_relax_heights', str(len(self.z)))
+        opt.set('namelist:idealised', 'num_mv_relax_times', '1')
+        opt.set('namelist:idealised', 'num_theta_relax_heights', str(len(self.z)))
+        opt.set('namelist:idealised', 'num_theta_relax_times', '1')
+
+        opt.set('namelist:idealised', 'theta_relax_data', ','.join(['{:.2f}'.format(v) for v in self.theta_profile]))
+        opt.set('namelist:idealised', 'theta_relax_height', ','.join(['{:.2f}'.format(v) for v in self.z]))
+        opt.set('namelist:idealised', 'theta_relax_time', '0')
+        opt.set('namelist:idealised', 'theta_relax_timescale', '25600.0')
+
+        opt.write(sys.stdout)
+        with open('rose-app-S0_relax.conf', 'w') as f:
+            opt.write(f)
 
     def save(self, state, suite):
         with open(self.task.output_filenames[0], 'w') as f:
