c050cf878ecd57d85bb26d1c17525678ad022162
Mark Muetzelfeldt <markmuetz@gmail.com>
2019-01-23 13:28:42 +0000

    Add in a new calculation to work out Flux equivalents of T_inc, mv_inc.

--- scaffold/suite/relaxation_plot.py
+++ scaffold/suite/relaxation_plot.py
@@ -1,9 +1,14 @@
+from logging import getLogger
+
 import matplotlib
 matplotlib.use('Agg')
 import pylab as plt
 
 from omnium import Analyser
 from omnium.utils import get_cube
+from omnium.consts import p_ref, kappa, L, cp, g
+
+logger = getLogger('scaf.rel_plot')
 
 
 class RelaxationPlot(Analyser):
@@ -25,27 +30,62 @@ class RelaxationPlot(Analyser):
             f.write('done')
 
     def display_results(self):
+        self._plot_theta_mv_incs()
+        self._calc_total_heating()
+
+    def _calc_total_heating(self):
+        total_heating = ['Expt,TrelFE [W m-2],QrelFE [W m-2]']
+        for expt in self.task.expts:
+            cube = self.expt_cubes[expt]
+
+            # incs on theta levels.
+            T_inc = get_cube(cube, 53, 181)
+            mv_inc = get_cube(cube, 53, 182)
+
+            # exnerp on rho levels.
+            exnerp = get_cube(cube, 0, 255)
+            p = exnerp.data ** (1 / kappa) * p_ref
+
+            # N.B. this is the correct way round if you want the right signs.
+            # TODO: this calculation is not exact. Midpoints of rho levels are not on theta
+            # levels so, working out dp should take this into account.
+            dp = (p[:, :-1] - p[:, 1:])
+
+            # z = T_inc.coord('level_height').points
+            # z_theta = exnerp.coord('level_height').points
+
+            # Note, I am ignoring top level!
+            T_inc_fe = ((cp / g) * (T_inc.data[:, :-1] / 30 * dp)).sum(axis=1).mean(axis=(1, 2))
+            q_inc = mv_inc.data / (1 - mv_inc.data)
+            q_inc_fe = ((L / g) * (q_inc[:, :-1] / 30 * dp)).sum(axis=1).mean(axis=(1, 2))
+
+            logger.debug('{} - T_inc flux equiv: {} W m-2', expt, T_inc_fe)
+            logger.debug('{} - q_inc flux equiv: {} W m-2', expt, q_inc_fe)
+            total_heating.append('{},{},{}'.format(expt, T_inc_fe.mean(), q_inc_fe.mean()))
+        self.save_text('relaxation_energy_flux.csv', '\n'.join(total_heating) + '\n')
+
+    def _plot_theta_mv_incs(self):
         plt.clf()
-        plt.xlabel('theta_inc (K day$^{-1}$)')
+        plt.xlabel('T_inc (K day$^{-1}$)')
         plt.ylabel('height (km)')
         for expt in self.task.expts:
-            theta_inc = get_cube(self.expt_cubes[expt], 53, 181)
+            T_inc = get_cube(self.expt_cubes[expt], 53, 181)
 
-            z = theta_inc.coord('level_height').points
+            z = T_inc.coord('level_height').points
             colour = None
 
             for i in range(8):
                 print(i)
                 # 2880 steps_per_perdiodm: converts to K/day
-                theta_inc_profile = theta_inc[i].data.mean(axis=(1, 2)) * 2880
+                T_inc_profile = T_inc[i].data.mean(axis=(1, 2)) * 2880
                 if i == 0:
-                    plot = plt.plot(theta_inc_profile, z / 1000, label=expt)
+                    plot = plt.plot(T_inc_profile, z / 1000, label=expt)
                     colour = plot[0].get_color()
                 else:
-                    plt.plot(theta_inc_profile, z / 1000, color=colour)
+                    plt.plot(T_inc_profile, z / 1000, color=colour)
 
         plt.legend()
-        plt.savefig(self.file_path('theta_incs.png'))
+        plt.savefig(self.file_path('T_incs.png'))
 
         plt.clf()
         plt.xlabel('mv_inc (g kg$^{-1}$ day$^{-1}$)')
