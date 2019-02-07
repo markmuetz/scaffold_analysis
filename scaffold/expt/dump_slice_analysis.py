1a9f2bdea5f1849a6ece0fd8ce3fc57ab915fd92
Mark Muetzelfeldt <markmuetz@gmail.com>
2019-02-07 17:00:46 +0000

    Rewrite dump_slice_analysis DRYly.
    Make some generic methods and use them for all vars with switches for differences.

--- scaffold/expt/dump_slice_analysis.py
+++ scaffold/expt/dump_slice_analysis.py
@@ -5,6 +5,7 @@ import numpy as np
 import scipy
 import matplotlib.pyplot as plt
 from matplotlib import colors
+import iris
 
 has_metpy = False
 try:
@@ -45,11 +46,18 @@ class DumpSliceAnalyser(Analyser):
     analysis_name = 'dump_slice_analysis'
     single_file = True
     input_dir = 'share/data/history/{expt}'
-    input_filename_glob = '{input_dir}/atmosa_da???.nc'
+    input_filename_glob = '{input_dir}/atmosa_da480.nc'
     output_dir = 'omnium_output/{version_dir}/{expt}'
     output_filenames = ['{output_dir}/atmos.{runid:03}.dump_slice_analysis.dummy']
     uses_runid = True
     runid_pattern = 'atmosa_da(?P<runid>\d{3}).nc'
+
+    def _create_cube(self, archetype, data, name, units):
+        cube = archetype.copy()
+        cube.rename(name)
+        cube.units = units
+        cube.data = data
+        return cube
 
     def load(self):
         self.load_cubes()
@@ -59,8 +67,8 @@ class DumpSliceAnalyser(Analyser):
         self.rho = get_cube(dump, 0, 253) / Re ** 2
         self.rho_d = get_cube(dump, 0, 389)
 
-        self.th = get_cube(dump, 0, 4)
-        self.ep = get_cube(dump, 0, 255)
+        self.theta = get_cube(dump, 0, 4)
+        self.exnerp = get_cube(dump, 0, 255)
 
         self.q = get_cube(dump, 0, 10)
         self.qcl = get_cube(dump, 0, 254)
@@ -77,21 +85,157 @@ class DumpSliceAnalyser(Analyser):
         except OmniumError:
             logger.info('dump has no qcf2')
 
+        qvdata = get_cube(dump, 0, 10).data
+        Tdata = self.theta.data * self.exnerp.data
+        pdata = self.exnerp.data ** (1 / kappa) * p_ref
+
+        p = pdata * units('Pa')
+        qv = qvdata * units('kg/kg')
+        T = Tdata * units('K')
+        Td = mpcalc.dewpoint_from_specific_humidity(qv, T, p)
+        theta_e = mpcalc.equivalent_potential_temperature(p, T, Td)
+
+        self.theta_e = self._create_cube(self.theta, theta_e.magnitude, 'theta_e', 'K')
+
     def display_results(self):
-        os.makedirs(self.file_path('/xy'), exist_ok=True)
-        os.makedirs(self.file_path('/xz'), exist_ok=True)
-        os.makedirs(self.file_path('/yz'), exist_ok=True)
         self.vertlevs = VertLev(self.suite.suite_dir)
         self._plot(self.task.expt)
 
+    def _create_dir_savefig(self, filename):
+        full_filename = self.file_path(filename)
+        os.makedirs(os.path.dirname(full_filename), exist_ok=True)
+        plt.savefig(full_filename)
+
     def _plot(self, expt):
-        self._w_slices_plots(expt)
-        self._w_mean_y_plots(expt)
-        self._th_slices_plots(expt)
-        self._u_slices_plots(expt)
-        self._qvar_slices_y_plots(expt)
-        self._qvar_mean_y_plots(expt)
-        self._th_e_slices_plots(expt)
+        # self._w_slices_plots(expt)
+        # self._w_mean_y_plots(expt)
+        # self._th_slices_plots(expt)
+        # self._u_slices_plots(expt)
+        # self._qvar_slices_y_plots(expt)
+        # self._qvar_mean_y_plots(expt)
+        # self._th_e_slices_plots(expt)
+#
+        self._plot_slices(expt, 'w', use_norm=True)
+        self._plot_slices(expt, 'theta', use_norm=True, anomaly=True)
+        self._plot_slices(expt, 'theta_e', use_norm=True, anomaly=True, cmap='RdBu_r')
+        qvars = ['qcl', 'qcf', 'qcf2', 'qrain', 'qgraup']
+        for qvar in qvars:
+            if not hasattr(self, qvar):
+                continue
+            self._plot_slices(expt, qvar, cmap='Blues')
+
+    def _plot_slices(self, expt, var, **kwargs):
+        cube = getattr(self, var)
+        z = cube.coord('atmosphere_hybrid_height_coordinate').points / 1000
+        self._plot_xy_slices(expt, var, cube, z, **kwargs)
+        self._plot_vert_slices(expt, var, cube, 'xz', **kwargs)
+        self._plot_vert_slices(expt, var, cube, 'yz', **kwargs)
+
+        self._plot_vert_mean_slice(expt, var, cube, 'xz', **kwargs)
+
+    def _plot_xy_slices(self, expt, var, cube, z, use_norm=False, cmap='bwr',
+                        anomaly=False):
+        for i in range(cube.shape[0]):
+            fig, ax = plt.subplots(dpi=100)
+            data = cube.data[i]
+            title = '{} xy slice at z={:.2f} km'.format(var, z[i])
+            if anomaly:
+                data = data - data.mean()
+                title = 'anom ' + title
+            ax.set_title(title)
+            # Coords are model_level, y, x or model_level, lat, lon
+            kwargs = {}
+            if use_norm:
+                kwargs['norm'] = MidpointNormalize(midpoint=0, vmin=data.min(), vmax=data.max())
+            if cmap:
+                kwargs['cmap'] = cmap
+            im = ax.imshow(data, origin='lower', **kwargs)
+
+            ax.set_xlabel('x (km)')
+            ax.set_ylabel('y (km)')
+            plt.colorbar(im)
+            self._create_dir_savefig('/xy/{2}/{0}_{1}_{2}_slice_{3}.png'
+                                     .format(expt, self.task.runid, var, i))
+            plt.close('all')
+
+    def _plot_vert_slices(self, expt, var, cube, plane='xy', use_norm=False, cmap='bwr',
+                          anomaly=False):
+        # Coords are model_level, y, x or model_level, lat, lon
+        if plane == 'xz':
+            N = cube.shape[1]
+        elif plane == 'yz':
+            N = cube.shape[2]
+        else:
+            raise ValueError('plane=[xz|yz]')
+
+        for i in range(N):
+            fig, ax = plt.subplots(dpi=100)
+            ax.set_ylabel('height (km)')
+            filename = ('/{0}/{3}/{1}_{2}_{3}_slice_{4}.png'
+                        .format(plane, expt, self.task.runid, var, i))
+            if plane == 'xz':
+                ax.set_xlabel('x (km)')
+                title = '{} xz slice at y={} gridbox'.format(var, i)
+                data = cube.data[:, i]
+            elif plane == 'yz':
+                ax.set_xlabel('y (km)')
+                title = '{} yz slice at x={} gridbox'.format(var, i)
+                data = cube.data[:, :, i]
+            if anomaly:
+                data = data - data.mean(axis=1)[:, None]
+                title = 'anom ' + title
+            ax.set_title(title)
+
+            self._plot_vert_slice(ax, data, N, use_norm, cmap)
+            self._create_dir_savefig(filename)
+            plt.close('all')
+
+    def _plot_vert_slice(self, ax, data, N, use_norm, cmap='bwr'):
+        data_rbs = scipy.interpolate.RectBivariateSpline(self.vertlevs.z_theta, np.arange(N),
+                                                         data)
+        data_interp = data_rbs(np.linspace(0, 40000, 400), np.linspace(0, N - 1, N))
+        kwargs = {}
+        if use_norm:
+            kwargs['norm'] = MidpointNormalize(midpoint=0, vmin=data.min(), vmax=data.max())
+        if cmap:
+            kwargs['cmap'] = cmap
+
+        im = ax.imshow(data_interp[:200], origin='lower', aspect=10, extent=(0, 256, 0, 20),
+                       **kwargs)
+
+        plt.colorbar(im)
+
+    def _plot_vert_mean_slice(self, expt, var, cube, plane='xy', use_norm=False, cmap='',
+                              anomaly=False):
+        # Coords are model_level, y, x or model_level, lat, lon
+        if plane == 'xz':
+            cube_index = 1
+        elif plane == 'yz':
+            cube_index = 2
+        else:
+            raise ValueError('plane=[xz|yz]')
+        N = cube.shape[cube_index]
+
+        fig, ax = plt.subplots(dpi=100)
+        ax.set_ylabel('height (km)')
+        filename = ('/{0}/{3}/{1}_{2}_{3}_mean.png'
+                    .format(plane, expt, self.task.runid, var))
+        data = cube.data.mean(axis=cube_index)
+        if plane == 'xz':
+            ax.set_xlabel('x (km)')
+            title = '{} xz mean'.format(var)
+        elif plane == 'yz':
+            ax.set_xlabel('y (km)')
+            title = '{} yz mean'.format(var)
+        if anomaly:
+            data = data - data.mean(axis=1)[:, None]
+            title = 'anom ' + title
+        ax.set_title(title)
+
+        # Coords are model_level, y, x or model_level, lat, lon
+        self._plot_vert_slice(ax, data, N, use_norm, cmap)
+        self._create_dir_savefig(filename)
+        plt.close('all')
 
     def _w_slices_plots(self, expt):
         wcube = self.w
