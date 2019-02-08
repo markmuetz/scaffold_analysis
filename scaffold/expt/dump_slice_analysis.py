cda05b3aa6622678faa7234fd3a217b08ff27241
Mark Muetzelfeldt <markmuetz@gmail.com>
2019-02-08 17:56:51 +0000

    Increase number of dump files.

--- scaffold/expt/dump_slice_analysis.py
+++ scaffold/expt/dump_slice_analysis.py
@@ -46,7 +46,7 @@ class DumpSliceAnalyser(Analyser):
     analysis_name = 'dump_slice_analysis'
     single_file = True
     input_dir = 'share/data/history/{expt}'
-    input_filename_glob = '{input_dir}/atmosa_da480.nc'
+    input_filename_glob = '{input_dir}/atmosa_da4??.nc'
     output_dir = 'omnium_output/{version_dir}/{expt}'
     output_filenames = ['{output_dir}/atmos.{runid:03}.dump_slice_analysis.dummy']
     uses_runid = True
