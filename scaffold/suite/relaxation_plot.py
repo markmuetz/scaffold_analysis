bfe35b5434300d09d10f0d7da696f0a8a1065685
Mark Muetzelfeldt <markmuetz@gmail.com>
2019-05-05 15:55:16 +0100

    Add a combined T/mv increment plot.

--- scaffold/suite/relaxation_plot.py
+++ scaffold/suite/relaxation_plot.py
@@ -1,11 +1,12 @@
 from logging import getLogger
+import warnings
 
 import matplotlib
 matplotlib.use('Agg')
 import pylab as plt
 
-from omnium import Analyser
-from omnium.utils import get_cube
+from omnium import Analyser, ExptList
+from omnium.utils import get_cube, cm_to_inch
 from omnium.consts import p_ref, kappa, L, cp, g, Re
 
 from scaffold.expt_settings import EXPT_DETAILS
@@ -34,25 +35,30 @@ class RelaxationPlot(Analyser):
     def display_results(self):
         self._plot_theta_mv_incs()
         self._plot_UCP_theta_mv_incs()
+        self._plot_combined_theta_mv_incs()
         self._calc_total_heating()
 
     def _calc_total_heating(self):
-        # total_heating = ['Expt,method,TrelFE [W m-2],QrelFE [W m-2]']
         total_heating = ['Expt,relFE [W m-2],QrelFE [W m-2]']
+        expts = ExptList(self.suite)
+        expts.find(self.task.expts)
         for expt in self.task.expts:
+            expt_obj = expts.get(expt)
+            dt = expt_obj.dt
+
             cube = self.expt_cubes[expt]
 
             # incs on theta levels.
             T_inc = get_cube(cube, 53, 181)
             mv_inc = get_cube(cube, 53, 182)
 
-            self._calc_vertical_integral(expt, cube, T_inc, mv_inc, total_heating)
-            # self._calc_vertical_integral_hydrostatic(expt, cube, T_inc, mv_inc, total_heating)
+            self._calc_vertical_integral(expt, cube, T_inc, mv_inc, dt, total_heating)
+            # self._calc_vertical_integral_hydrostatic(expt, cube, T_inc, mv_inc, dt, total_heating)
 
         self.save_text('final_day_relaxation_energy_flux.csv', '\n'.join(total_heating) + '\n')
         self.save_text('final_day_relaxation_energy_flux.csv.done', 'done')
 
-    def _calc_vertical_integral(self, expt, cube, T_inc, mv_inc, total_heating):
+    def _calc_vertical_integral(self, expt, cube, T_inc, mv_inc, dt, total_heating):
         rho = get_cube(cube, 0, 253)
         rho.data = rho.data / Re**2
         for coord in T_inc.coords():
@@ -64,34 +70,40 @@ class RelaxationPlot(Analyser):
                 break
         z_coord = T_inc.coord(height_name)
         dz = z_coord.bounds[:, 1] - z_coord.bounds[:, 0]
-        T_inc_fe = (cp * (rho.data * T_inc.data / 30 * dz[None, :, None, None])).sum(axis=1).mean(axis=(1, 2))
+        T_inc_fe = (cp * (rho.data * T_inc.data / dt * dz[None, :, None, None])).sum(axis=1).mean(axis=(1, 2))
         q_inc = mv_inc.data / (1 - mv_inc.data)
-        q_inc_fe = (L * (rho.data * q_inc / 30 * dz[None, :, None, None])).sum(axis=1).mean(axis=(1, 2))
+        q_inc_fe = (L * (rho.data * q_inc / dt * dz[None, :, None, None])).sum(axis=1).mean(axis=(1, 2))
         logger.debug('{} - T_inc flux equiv: {} W m-2', expt, T_inc_fe)
         logger.debug('{} - q_inc flux equiv: {} W m-2', expt, q_inc_fe)
         # total_heating.append('{},nonhydrostatic,{},{}'.format(expt, T_inc_fe.mean(), q_inc_fe.mean()))
         total_heating.append('{},{},{}'.format(expt, T_inc_fe.mean(), q_inc_fe.mean()))
 
-    def _calc_vertical_integral_hydrostatic(self, expt, cube, T_inc, mv_inc, total_heating):
+    def _calc_vertical_integral_hydrostatic(self, expt, cube, T_inc, mv_inc, dt, total_heating):
+        msg = ("This method has some incorrect assumptions in it\n"
+               "Please use _calc_vertical_integral instead")
+        warnings.warn(msg, DeprecationWarning)
+        # See ASSUMPTION
+        # N.B. difference is small, but using the correct calc leads to better cons. of energy in
+        # budget calcs.
+
         # exnerp on rho levels.
         exnerp = get_cube(cube, 0, 255)
         p = exnerp.data ** (1 / kappa) * p_ref
+
+        # ASSUMPTION: this calculation is not exact. Midpoints of rho levels are not on theta
+        # ASSUMPTION: this calculation also assumes hydrostatic equilibrium - this is not right
+        # ASSUMPTION: this calculation also doesn't take into account theta-level 0 - not sure about?
         # N.B. this is the correct way round if you want the right signs.
-        # TODO: this calculation is not exact. Midpoints of rho levels are not on theta
-        # TODO: this calculation also assumes hydrostatic equilibrium - this is not right
-        # TODO: this calculation also doesn't take into account theta-level 0 - not sure about?
-        # levels so, working out dp should take this into account.
         dp = (p[:, :-1] - p[:, 1:])
         # z = T_inc.coord('level_height').points
         # z_theta = exnerp.coord('level_height').points
-        # TODO: Uses hydrostatic approx which is not valid. Do proper dz integral.
         # Note, I am ignoring top level!
-        T_inc_fe = ((cp / g) * (T_inc.data[:, :-1] / 30 * dp)).sum(axis=1).mean(axis=(1, 2))
+        T_inc_fe = ((cp / g) * (T_inc.data[:, :-1] / dt * dp)).sum(axis=1).mean(axis=(1, 2))
         q_inc = mv_inc.data / (1 - mv_inc.data)
-        q_inc_fe = ((L / g) * (q_inc[:, :-1] / 30 * dp)).sum(axis=1).mean(axis=(1, 2))
+        q_inc_fe = ((L / g) * (q_inc[:, :-1] / dt * dp)).sum(axis=1).mean(axis=(1, 2))
         logger.debug('{} - T_inc flux equiv: {} W m-2', expt, T_inc_fe)
         logger.debug('{} - q_inc flux equiv: {} W m-2', expt, q_inc_fe)
-        total_heating.append('{},hydrostatic,{},{}'.format(expt, T_inc_fe.mean(), q_inc_fe.mean()))
+        total_heating.append('{},{},{}'.format(expt, T_inc_fe.mean(), q_inc_fe.mean()))
 
     def _plot_theta_mv_incs(self):
         plt.clf()
@@ -176,3 +188,41 @@ class RelaxationPlot(Analyser):
         plt.ylim((0, 15))
         plt.legend(loc='upper right')
         plt.savefig(self.file_path('UCP_mv_incs.png'))
+
+    def _plot_combined_theta_mv_incs(self):
+        fig, (ax1, ax2) = plt.subplots(1, 2, sharey=True, figsize=cm_to_inch(16, 12))
+
+        ax1.set_xlabel('T_inc (K day$^{-1}$)')
+        ax1.set_ylabel('height (km)')
+        for expt in self.task.expts:
+            kwargs = {}
+            if expt in EXPT_DETAILS:
+                kwargs = dict(zip(['label', 'color', 'linestyle'], EXPT_DETAILS[expt]))
+            T_inc = get_cube(self.expt_cubes[expt], 53, 181)
+
+            z = T_inc.coord('level_height').points
+
+            # 2880 steps_per_perdiodm: converts to K/day
+            T_inc_profile = T_inc.data.mean(axis=(0, 2, 3)) * 2880
+            ax1.plot(T_inc_profile, z / 1000, **kwargs)
+
+        ax1.set_xlim((-4, 1))
+        ax1.set_ylim((0, 15))
+        ax1.axvline(x=0, ls='--', color='k')
+        ax2.set_xlabel('mv_inc (g kg$^{-1}$ day$^{-1}$)')
+        for expt in self.task.expts:
+            kwargs = {}
+            if expt in EXPT_DETAILS:
+                kwargs = dict(zip(['label', 'color', 'linestyle'], EXPT_DETAILS[expt]))
+            mv_inc = get_cube(self.expt_cubes[expt], 53, 182)
+            z = mv_inc.coord('level_height').points
+
+            # 2880 steps_per_perdiodm: converts to g/kg/day
+            mv_inc_profile = mv_inc.data.mean(axis=(0, 2, 3)) * 2880 * 1000
+            ax2.plot(mv_inc_profile, z / 1000, **kwargs)
+
+        ax2.set_xlim((-1, 0.4))
+        ax2.set_ylim((0, 15))
+        ax2.axvline(x=0, ls='--', color='k')
+        ax2.legend(loc='upper left')
+        plt.savefig(self.file_path('combined_theta_mv_incs.png'))
